5850a8788dc4432ba5edaeca3e484f2b744beeec
Divya Tyagi <tyagi.divya08@gmail.com>
2023-10-20 11:46:03 -0400

    q1 complete

---
+++ q1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    std::vector<int> SCE_PHL; // define vectors
+    std::vector<int> SCE_ORD;
+    std::vector<int> SCE_EWR;
+
+    SCE_PHL.push_back(160);
+    SCE_ORD.push_back(640);
+    SCE_EWR.push_back(220);
+}
