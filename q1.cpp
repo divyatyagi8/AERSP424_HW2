20812d1359156257e79058ba069c9a0e8a26f6ab
Divya Tyagi <tyagi.divya08@gmail.com>
2023-10-25 23:50:51 -0400

    updates to q1-3

--- q1.cpp
+++ q1.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 #include <vector>
+#include "q1.h"
 
 using namespace std;
 
 int main()
 {
-    std::vector<int> SCE_PHL; // define vectors
-    std::vector<int> SCE_ORD;
-    std::vector<int> SCE_EWR;
-
-    SCE_PHL.push_back(160);
-    SCE_ORD.push_back(640);
-    SCE_EWR.push_back(220);
+    cout<<SCE_PHL[0]<<endl;
+    cout<<SCE_ORD[0]<<endl;
+    cout<<SCE_EWR[0]<<endl;
 }
