886aaa8156306be82ed597bd0f2b489ca9ec9de9
Divya Tyagi <tyagi.divya08@gmail.com>
2023-11-07 20:58:17 -0500

    updates to q1-7 11_7

--- hw2_submission.cpp
+++ hw2_submission.cpp
@@ -24,21 +24,15 @@ public:
 
     Plane() // default constructor
     {
-        origin = "";
+        origin = ""; // define as empty strings
         destination = "";
-        distance = 0.0;
-        pos = 0.0;
-        vel = 0.0;
-        wait_time = 0.0;
-        loiter_time = 0.0;
-        at_SCE = false;
-    }
-    Plane(string& from, string& to) // constructor
+    } 
+
+    Plane(const string& from, const string& to) // constructor
     {
         origin = from;
         destination = to;
-        
-        distance = calc_dist();
+        distance = calc_dist(); // function to calc. distances between various airports
 
             pos = 0.0;
             vel = 0.0;
@@ -71,7 +65,7 @@ public:
     }
     virtual ~Plane() // virtual deconstuctor
     {
-
+        // empty
     }
     double getpos()
     {
@@ -127,7 +121,7 @@ public:
         std::random_device rd;
         std::mt19937 gen(rd());
         std::normal_distribution<double> dist(mean, stdev);
-        return dist(gen);
+        return dist(gen); // random # generator between set range
     }
 
     void operate(double dt)
@@ -180,34 +174,36 @@ private:
     string Airline;
 
 public:
-    Airliner(const string& airline, const string& from, const string& to)
+    Airliner(const string& airline, const string& from, const string& to) : Plane(from, to) // constructor
         { 
             Airline = airline;
         }
 
-    // Destructor with an empty implementation
-    ~Airliner() {}
 
-    // Override plane_type to return the airline
-    string plane_type() override {
+    ~Airliner() {} // empty destructor
+
+
+    string plane_type() override // override function
+    {
         return Airline;
     }
 
-    // Override time_on_ground to draw wait_time with mean 1800 and standard deviation 600
-    void time_on_ground() override {
-        wait_time = draw_from_normal_dist(1800, 600);
+
+    void time_on_ground() override // override function
+    { 
+        wait_time = draw_from_normal_dist(1800, 600); // generate random wait time
     }
 };
 
 class GeneralAviation : public Plane 
 {
     public:
-    GeneralAviation(const string& from, const string& to)
+    GeneralAviation(const string& from, const string& to) : Plane(from, to) // constructor
     {
     }
-    ~GeneralAviation(){}
+    ~GeneralAviation(){} // empty destructor
 
-    void time_on_ground() override
+    void time_on_ground() override // override function
     {
         wait_time = draw_from_normal_dist(600,60);
     }
@@ -222,24 +218,22 @@ private:
     const int AIRSPACE_DISTANCE = 50;
 
 public:
-    ATC() {
-        // Constructor sets the values for MAX_LANDED_PLANE_NUM and AIRSPACE_DISTANCE
+    ATC() // empty constructor
+    {
     }
 
-    ~ATC() {
-        // Destructor (empty implementation)
+    ~ATC() // empty destructor
+    {
     }
 
-    void register_plane(Plane* plane) {
+    void register_plane(Plane* plane) 
+    {
         if (registered_planes.size() < MAX_LANDED_PLANE_NUM) {
-            // Check if the maximum number of planes allowed to land has not been reached
             registered_planes.push_back(plane);
-            cout << "Plane registered with ATC." << endl;
-        } else {
-            cout << "ATC: Maximum number of planes allowed to land has been reached." << endl;
+        } else 
+        {
         }
     }
-
 
     void control_traffic()
     {
@@ -251,16 +245,12 @@ public:
             landed_planes += a_plane->getat_SCE();
             i++;
 
-            //if statement to determine if landed planes is greater than or equal to the following will be set
             if (landed_planes >= MAX_LANDED_PLANE_NUM) 
             {
-                i = 0; //set i equal to zero
+                i = 0; // initialize i
 
-                //for loop to determine if i is less than registered planes then the if stetment will be set
                 for (Plane* b_plane : registered_planes) 
                 {
-
-                    //if statement to set the loiter time and i++ values under specific conditions
                     if (b_plane->getat_SCE() == 0 && b_plane->distance_to_SCE() <= ATC::AIRSPACE_DISTANCE && b_plane->getloiter_time() == 0) 
                     {
                         b_plane->setloiter_time(100);
@@ -272,21 +262,19 @@ public:
     }
 };
 
-
-
-int main()
+ int main( int argc, char** argv ) 
 {
 
-   // cout << SCE_PHL[0] << endl;
-    //cout << SCE_ORD[0] << endl;
-   // cout << SCE_EWR[0] << endl;
+    // cout << SCE_PHL[0] << endl; // cout statements for initial containers from Q1
+    // cout << SCE_ORD[0] << endl;
+    // cout << SCE_EWR[0] << endl;
 
 // Q5
-    // Create an array of pointers to the base class
-    const int num_aircraft = 7;
-    Plane* aircraft[num_aircraft];
 
-    // Instantiate aircraft based on your specifications
+    const int num_aircraft = 7;
+    Plane* aircraft[num_aircraft]; // create array to hold aircraft
+
+    // instantiate aircraft based on table in Q5
     aircraft[0] = new Airliner("AA", "SCE", "PHL");
     aircraft[1] = new Airliner("UA", "SCE", "ORD");
     aircraft[2] = new Airliner("UA", "SCE", "EWR");
@@ -295,20 +283,18 @@ int main()
     aircraft[5] = new GeneralAviation("SCE", "EWR");
     aircraft[6] = new GeneralAviation("SCE", "ORD");
 
-    aircraft[0]->setvel(470);  // mph
-    aircraft[1]->setvel(515);
-    aircraft[2]->setvel(480);
-    aircraft[3]->setvel(500);
-    aircraft[4]->setvel(140);
-    aircraft[5]->setvel(160);
-    aircraft[6]->setvel(180);
+    aircraft[0]->setvel(0.130555556);  // miles per second
+    aircraft[1]->setvel(0.143055556);
+    aircraft[2]->setvel(0.133333333);
+    aircraft[3]->setvel(0.138888889);
+    aircraft[4]->setvel(0.0388888889);
+    aircraft[5]->setvel(0.0444444444);
+    aircraft[6]->setvel(0.05);
 
 
-    // Simulation parameters
-    const double timeStep = 100;
-
+    const double timeStep = 100; // choose time step
     double currentTime = 0.0;
-    double endTime = 1440.0; // Simulation runs for 24 hours
+    double endTime = 1440.0; // 24 hr sim
 
     ATC atc; // instantiate ATC object
 
@@ -317,9 +303,8 @@ int main()
         atc.register_plane(aircraft[i]);
     }
 
-    // print out pos of airplane at each time step
-    while (currentTime < endTime) {
-        
+
+    while (currentTime < endTime) { // while statement
         for (int i = 0; i < num_aircraft; i++) {
             aircraft[i]->operate(timeStep);
             atc.control_traffic();
